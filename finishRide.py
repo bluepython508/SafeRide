6574042c9a3051931534b8aed5aaee269ab2b699
bluepython508 <bluepython508@gmail.com>
2017-10-31 11:31:41 +0000

    Restructured copying and video processing

--- finishRide.py
+++ finishRide.py
@@ -6,7 +6,8 @@ from os import listdir, makedirs
 import shelve
 import datetime
 # import time
-from shutil import copytree
+from shutil import copy
+
 
 class AlprError(Exception):
     pass
@@ -15,7 +16,11 @@ class AlprError(Exception):
 def main():
     ride = datetime.datetime.now().strftime('/mnt/%Y/%m/%d-%H/')
     makedirs(ride, exist_ok=True)
-    copytree('/home/video/SafetyVideo', ride)
+    all_videos = listdir('/home/video/SafetyVideo/FrontPi')
+    for video in all_videos:
+        makedirs(ride + video.replace('.h264', ''), exist_ok=True)
+        copy('/home/video/SafetyVideo/FrontPi/' + video, ride + video.replace('.h264', '/') + 'FrontPi.h264')
+        copy('/home/video/SafetyVideo/SidePi/' + video, ride + video.replace('.h264', '/') + 'SidePi.h264')
     shelf = shelve.open(ride + 'data', writeback=True)
     mainshelf = shelve.open('/mnt/data', writeback=True)
     mainshelf['rides'].append(ride.replace('/mnt/', ''))
@@ -25,24 +30,23 @@ def main():
     alpr = Alpr('eu', '/etc/openalpr/openalpr.conf', '/usr/share/openalpr/runtime_data')
     if not alpr.is_loaded():
         raise AlprError('Couldn\'t load OpenALPR.')
-    for pi in ('/home/video/SafetyVideo/SidePi', '/home/video/SafetyVideo/FrontPi'):
-        # TODO: Restructure so processing both videos at same time
-        videos = listdir(pi) + listdir(o)
-        for video in videos:
-            with closing(VideoSequence(video)) as sequence:
-                oldlicense = None
-                for frame in sequence:
-                    try:
-                        plate = alpr.recognise_ndarray(frame.to_array())['results'][0]['candidates']['plate']
-                    except:
-                        plate = ''
-                    if not plate == oldlicense:
-                        shelf['licenses'][video] = plate
-                        break
-                    oldlicense = plate
+    videos = listdir('/home/video/SafetyVideo/FrontPi')
+    for video in videos:
+        with closing(VideoSequence(video)) as sequence:
+            oldlicense = None
+            for frame in sequence:
+                try:
+                    plate = alpr.recognise_ndarray(frame.to_array())['results'][0]['candidates']['plate']
+                except:
+                    plate = ''
+                if not plate == oldlicense:
+                    shelf['licenses'][video.replace('.h264', '')] = plate
+                    break
+                oldlicense = plate
 
     shelf.sync()
     shelf.close()
+
 
 if __name__ == "__main__":
     main()
