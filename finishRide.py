67089f543bd9d15caf3457eaa5dfeda1c74f3f61
bluepython508 <bluepython508@gmail.com>
2017-11-05 16:09:09 +0000

    Updated ride dict storage

--- finishRide.py
+++ finishRide.py
@@ -13,6 +13,18 @@ class AlprError(Exception):
     pass
 
 
+def ordinal_number(number):
+    if str(number)[-1] == '1' and not (number == 11):
+        suffix = "st"
+    elif str(number)[-1] == '2' and not (number == 12):
+        suffix = "nd"
+    elif str(number)[-1] == '3' and not (number == 13):
+        suffix = "rd"
+    else:
+        suffix = "th"
+    return str(number) + suffix
+
+
 def main():
     ride = datetime.datetime.now().strftime('/mnt/%Y/%m/%d-%H/')
     makedirs(ride, exist_ok=True)
@@ -23,7 +35,16 @@ def main():
         copy('/home/video/SafetyVideo/SidePi/' + video, ride + video.replace('.h264', '/') + 'SidePi.h264')
     shelf = shelve.open(ride + 'data', writeback=True)
     mainshelf = shelve.open('/mnt/data', writeback=True)
-    mainshelf['rides'].append(ride.replace('/mnt/', ''))
+    now = datetime.datetime.now()
+    ride_data = {'href': ride.replace('/mnt/', '/ride/'), 'date': ordinal_number(now.day)}
+    if not 'rides' in mainshelf:
+        mainshelf['rides'] = {}
+    if not now.year in mainshelf['rides']:
+        mainshelf['rides'][now.year] = {now.month: [ride_data]}
+    elif not now.month in mainshelf['rides'][now.year]:
+        mainshelf['rides'][now.year][now.month] = [ride_data]
+    else:
+        mainshelf['rides'][now.year][now.month].append(ride_data)
     mainshelf.sync()
     mainshelf.close()
     shelf['incidents'] = []
