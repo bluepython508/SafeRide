67829e744c0be306e10f7dfe40337e5915c8f399
Your Name <you@example.com>
2018-01-06 20:29:46 +0000

    Fixed SafeRide code

--- SidePi.py
+++ SidePi.py
@@ -17,51 +17,48 @@ DEFAULT_OUTPUT_DIR = "/home/video/SafetyVideo"
 DEFAULT_WIFI_SSID = "SafeRide"
 DEFAULT_SERVER_HOSTNAME = "saferide.local"
 DEFAULT_SAVE_DIR = "/home/pi/Video"
+DEFAULT_CHIP_SELECT = 25
+DEFAULT_MOSI = 22
+DEFAULT_MISO = 10
+DEFAULT_CLK = 9
 
-
-class LoopExit(Exception):
-    pass
-
-
-def exitLoop():
-    raise LoopExit()
-
-
-def getDistance(val):
-    mv = (val * 5) * 1000  # 0-1 x5-> 0-5 Volts -> x1000 = mV
-    distance_mm = 1420000 / (mv - 1091)
-    return distance_mm / 10  # Cm
-
+def getDistance(x):
+        return 3714 * x ** 2 - 4212* x + 1275
 
 def threshholdIter(values, threshhold, operation=lambda x: x):
     for val in values:
         value = operation(val)
-        yield 0 if (value < threshhold) else 1
+        #print(val)
+        #print(value)
+        yield 0 if (value > threshhold) else 1
 
 
 def runIter(values, func, *args, **kwargs):
     for value in values:
-        if value:
-            thread = threading.Thread(target=func, args=args, kwargs=kwargs)
-            thread.start()
+        if value > 0:
+            #thread = threading.Thread(target=func, args=args, kwargs=kwargs)
+            #thread.start()
+            func(*args, **kwargs)
         yield value
 
 
 class SidePi:
     def __init__(self, args):
         self.args = args
-
+        print('init')
         self.flag_button = gpiozero.Button(2)
         self.ride_button = gpiozero.Button(21)
         self.flag_signal = gpiozero.LED(3)
         self.pass_signal = gpiozero.LED(4)
         self.ride_signal = gpiozero.LED(5)
-        self.range_sensor = gpiozero.MCP3008()
+        self.range_sensor = gpiozero.MCP3008(select_pin=args.chip_select, mosi_pin=args.mosi, miso_pin=args.miso, clock_pin=args.clk)
         self.recording_led = gpiozero.LED(20)
-        self.ride_button.when_pressed = lambda: self.startRide()
+        self.ride_button.wait_for_press()
+        self.startRide()
 
     def finishRide(self):
-        subprocess.run(("wifi", "connect", "--ad-hoc"), self.args.ssid)  # Connect to WiFi
+        print('Finished')
+        subprocess.run(("wifi", "connect", "--ad-hoc", self.args.network))  # Connect to WiFi
         # Copy video over to a unique filename
         try:
             subprocess.run(
@@ -75,42 +72,45 @@ class SidePi:
         self.flag_signal.on()
         time.sleep(0.01)
         self.flag_signal.off()
-        requests.get('{}/rideDone'.format(self.args.server))
+        requests.get('http://{}/rideDone'.format(self.args.server))
         subprocess.run(("poweroff",))  # Shutdown
 
     def onPass(self):
-        self.recording_led.on()
+        print('Unsafe pass')
+        self.recording_led.blink(on_time=self.args.pass_length, off_time=0, n=1, background=True)
         self.stream.clear()
         self.camera.wait_recording(self.args.pass_length)
+        print(self.get_path())
         self.stream.copy_to(self.get_path(), seconds=self.args.pass_length, first_frame=picamera.PiVideoFrameType.frame)
-        self.recording_led.off()
 
     def onFlag(self):
+        print('Flag')
         self.recording_led.blink(background=True, n=1)
         self.flag_signal.blink(on_time=0.1, background=True, n=1)
+        print(self.get_path())
         self.stream.copy_to(self.get_path(), seconds=self.args.flag_length, first_frame=picamera.PiVideoFrameType.frame)
 
     def startRide(self):
+        print('Starting ride')
         self.camera = picamera.PiCamera(sensor_mode=2)
         self.stream = picamera.PiCameraCircularIO(self.camera,
                                                   seconds=max(self.args.flag_length, self.args.pass_length))
         self.camera.start_recording(self.stream, format="h264")
         self.pass_signal.source = runIter(threshholdIter(self.range_sensor.values, 150, getDistance), self.onPass)
-        self.ride_button.when_pressed = exitLoop
-        self.flag_button.when_pressed = self.onFlag
-        try:
-            while True:
-                self.camera.wait_recording(1)
-        except LoopExit:
-            pass
-        finally:
-            self.camera.stop_recording()
-            self.camera.close()
-            self.ride_button.when_pressed = lambda: self.startRide()
-            self.flag_button.when_pressed = lambda: self.finishRide()
+        self.flag_button.when_activated = lambda: self.onFlag()
+        self.ride_button.wait_for_release()
+        self.ride_button.wait_for_press()
+        self.camera.stop_recording()
+        self.camera.close()
+        print("Ride done")
+        self.flag_button.when_pressed = lambda: self.finishRide()
+        self.pass_signal.source = None
+        self.ride_button.wait_for_release()
+        self.ride_button.wait_for_press()
+        self.startRide()
 
     def get_path(self):
-        self.args.save_dir + "/{}.mp4".format(time.strftime('%Y-%m-%dT%H:%M:%S'))
+        return self.args.save_dir + "/{}.mp4".format(time.strftime('%Y-%m-%dT%H:%M:%S'))
     
     
 if __name__ == "__main__":
@@ -121,7 +121,13 @@ if __name__ == "__main__":
     parser.add_argument("-n", "--network", default=DEFAULT_WIFI_SSID)
     parser.add_argument("-s", "--server", default=DEFAULT_SERVER_HOSTNAME)
     parser.add_argument("-d", "--save-dir", default=DEFAULT_SAVE_DIR)
-
+    parser.add_argument("--chip-select", default=DEFAULT_CHIP_SELECT)
+    parser.add_argument("--miso", default=DEFAULT_MISO)
+    parser.add_argument("--mosi", default=DEFAULT_MOSI)
+    parser.add_argument("--clk", default=DEFAULT_CLK)
+    
+    
     args = parser.parse_args()
     pi = SidePi(args)
+    print(pi.get_path())
     signal.pause()
